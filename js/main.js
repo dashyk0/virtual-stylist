edf8b43c148cc3e788d01be2345e27acd62516c4
dashyk0 <dasapusina111@gmail.com>
2026-03-01 22:47:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 // main.js
 document.addEventListener('DOMContentLoaded', () => {
-  // Подгрузка хэдера
-  fetch('../includes/header.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('header-placeholder').innerHTML = html;
-    });
+ // Подгрузка хэдера
+fetch('/virtual-stylist/includes/header.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('header-placeholder').innerHTML = html;
+  })
+  .catch(err => console.error('Ошибка загрузки хэдера:', err));
 
-  // Подгрузка футера
-  fetch('../includes/footer.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('footer-placeholder').innerHTML = html;
-    });
-});
+// Подгрузка футера
+fetch('/virtual-stylist/includes/footer.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('footer-placeholder').innerHTML = html;
+  })
+  .catch(err => console.error('Ошибка загрузки футера:', err));
